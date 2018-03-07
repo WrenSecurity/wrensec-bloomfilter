09e9f6bb46f560de58b09250705cf09f61385a37
Kortanul <kortanul@protonmail.com>
2018-03-06 23:35:43 -0500

    Fix Javadoc Errors that Block Build on JDK8

--- bloomfilter-core/src/main/java/org/forgerock/bloomfilter/BloomFilter.java
+++ bloomfilter-core/src/main/java/org/forgerock/bloomfilter/BloomFilter.java
@@ -12,6 +12,7 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyright 2015 ForgeRock AS.
+ * Portions Copyright 2018 Wren Security.
  */
 
 package org.forgerock.bloomfilter;
@@ -19,29 +20,32 @@ package org.forgerock.bloomfilter;
 import java.util.Collection;
 
 /**
- * General interface contract for implementations of <a href="http://en.wikipedia.org/wiki/Bloom_filter">Bloom
- * Filters</a>. A Bloom Filter is a Set-like data structure that uses only a few bits of memory per element stored.
- * This allows very large numbers (billions) of elements to be stored in-memory, with the trade-off being that set
- * containment can now return false positives. That is, the Bloom Filter can say for certain if a given element is
- * definitely <em>not</em> in the set, but it cannot say for certain whether it is. Bloom Filters are therefore
- * appropriate as a quick initial check to reduce load on a more expensive but more accurate storage mechanism (e.g.,
- * a database) or where some level of false positives is tolerable. The expected usage pattern for the former case
- * would be something like the following psuedo-code:
- * <pre>
- *     BloomFilter&lt;T&gt filter = ...;
- *     Set&lt;T&gt; expensiveSet = ...; // e.g. database, web-service
- *     if (filter.mightContain(element)) {
- *         // Perform the more expensive check to be sure
- *         return expensive.contains(element);
- *     }
- *     return false;
+ * General interface contract for implementations of Bloom Filters.
+ *
+ * <p>A Bloom Filter is a Set-like data structure that uses only a few bits of memory per element
+ * stored. This allows very large numbers (billions) of elements to be stored in-memory, with the
+ * trade-off being that set containment can now return false positives. That is, the Bloom Filter
+ * can say for certain if a given element is definitely <em>not</em> in the set, but it cannot say
+ * for certain whether it is. Bloom Filters are therefore appropriate as a quick initial check to
+ * reduce load on a more expensive but more accurate storage mechanism (e.g., a database) or where
+ * some level of false positives is tolerable. The expected usage pattern for the former case would
+ * be something like the following psuedo-code:
+ * <pre>{@code
+ *   BloomFilter<T> filter = ...;
+ *   Set<T> expensiveSet = ...; // e.g. database, web-service
+ *   if (filter.mightContain(element)) {
+ *     // Perform the more expensive check to be sure
+ *     return expensive.contains(element);
+ *   }
+ *   return false;
+ * }
  * </pre>
- * Note: this assumes that the Bloom Filter is kept in-sync with the definitive set! How this is accomplished is
- * outside of the scope of this package.
- * <p/>
- * All Bloom Filter implementations in this package allow the probability of false positives (FPP) to be specified,
- * and the implementation will adjust the amount of memory used to ensure the given level of accuracy for the
- * expected number of elements.
+ * Note: this assumes that the Bloom Filter is kept in-sync with the definitive set! How this is
+ * accomplished is outside of the scope of this package.
+ *
+ * <p>All Bloom Filter implementations in this package allow the probability of false positives
+ * (FPP) to be specified, and the implementation will adjust the amount of memory used to ensure the
+ * given level of accuracy for the expected number of elements.
  *
  * @param <E> the type of elements contained in the bloom filter.
  * @see <a href="http://en.wikipedia.org/wiki/Bloom_filter">Bloom Filter Wikipedia entry</a>.
@@ -49,8 +53,9 @@ import java.util.Collection;
 public interface BloomFilter<E> {
 
     /**
-     * Adds the specified element to this set if it is not already possibly present. After a call to this method,
-     * subsequent calls to {@link #mightContain(Object)} will return {@code true} for the same object.
+     * Adds the specified element to this set if it is not already possibly present. After a call to
+     * this method, subsequent calls to {@link #mightContain(Object)} will return {@code true} for
+     * the same object.
      *
      * @param element the element to add to this set.
      */
@@ -64,12 +69,14 @@ public interface BloomFilter<E> {
     void addAll(Collection<? extends E> elements);
 
     /**
-     * Checks if the given element <em>might</em> be a member of this set. If this method returns {@code false}, then
-     * the given object is definitely not a member of the set. If the result is {@code true} then the object may or
-     * may not be a member of this set, with a certain probability of false positives.
+     * Checks if the given element <em>might</em> be a member of this set. If this method returns
+     * {@code false}, then the given object is definitely not a member of the set. If the result is
+     * {@code true} then the object may or may not be a member of this set, with a certain
+     * probability of false positives.
      *
      * @param element the element to check for membership in this set.
-     * @return {@code false} if the element is definitely not in the set, or {@code true} if it might be.
+     * @return {@code false} if the element is definitely not in the set, or {@code true} if it
+     *         might be.
      */
     boolean mightContain(E element);
 
